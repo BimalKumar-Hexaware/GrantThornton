754f784d69b31a3341cffc83df7b87c577c55274
Bimal Kumar <40321@hexaware.com>
2018-09-26 14:47:25 +0530

    ss

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.post('/api/webhook', (req, res) => {
     console.log("Dialogflow request body", JSON.stringify(req.body));
