b36f810f6de7739f9e78cd672e9376f9a2061471
Bimal Kumar <40321@hexaware.com>
2018-09-27 16:49:52 +0530

    c

--- controller.js
+++ controller.js
@@ -1,7 +1,10 @@
+var helper = require('./helper');
 module.exports = {
     "webhookRequestHandler": (req, res) => {
         console.log("Dialogflow request body", JSON.stringify(req.body));
         console.log("DF Action", req.body.queryResult.action);
+        var parameters = (typeof req.body.queryResult.outputContexts[0].parameters !== "undefined") ? req.body.queryResult.outputContexts[0].parameters : {};
+        console.log("Parameters", parameters);
         switch (req.body.queryResult.action) {
             case "gt.userquery-applyfilter-date-supplydate":
                 res.json({
@@ -18,17 +21,33 @@ module.exports = {
                 });
                 break;
             case "gt.userquery-applyfilter-revenue-ranges":
-                res.json({
-                    "fulfillmentMessages": [
-                        {
-                            "text": {
-                                "text": [
-                                    "push to api"
-                                ]
-                            },
-                            "platform": "SKYPE"
-                        }
-                    ]
+                return helper.callDynamicsAPI(parameters).then((result) => {
+                    res.json({
+                        "fulfillmentMessages": [
+                            {
+                                "text": {
+                                    "text": [
+                                        "api call success"
+                                    ]
+                                },
+                                "platform": "SKYPE"
+                            }
+                        ]
+                    });
+                }).catch((err) => {
+                    console.log("some error occured");
+                    res.json({
+                        "fulfillmentMessages": [
+                            {
+                                "text": {
+                                    "text": [
+                                        "Sorry, something went wrong"
+                                    ]
+                                },
+                                "platform": "SKYPE"
+                            }
+                        ]
+                    });
                 });
                 break;
         }
--- helper.js
+++ helper.js
@@ -1,4 +1,28 @@
+var request = require('request');
+
 var self = {
+    "callDynamicsAPI": function (params) {
+        console.log('inside callDynamicsAPI');
+        return new Promise(function (resolve, reject) {
+            var options = {
+                method: 'POST',
+                url: 'http://ec2-18-232-207-49.compute-1.amazonaws.com:7500/opportunities',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: params,
+                json: true
+            };
+            request(options, function (error, response, body) {
+                if (error) {
+                    console.log("callDynamicsAPI err", error);
+                    reject("Something went wrong when processing your request. Please try again.");
+                }
+                console.log("callDynamicsAPI result body", body);
+                resolve(body);
+            });
+        });
+    }
 
 };
 
