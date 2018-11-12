6f57a33fa4a50a0cd35b52bab323a7a677d20e1e
Bimal Kumar <40321@hexaware.com>
2018-11-12 14:37:38 +0530

    sss

--- controller.js
+++ controller.js
@@ -8,7 +8,7 @@ module.exports = {
         console.log("DF Action", req.body.queryResult.action);
         var params = {};
         switch (req.body.queryResult.action) {
-            case "input.welcome":
+            case "gt.startConversation":
                 res.json({
                     "fulfillmentMessages": [{
                         "text": {
@@ -47,11 +47,11 @@ module.exports = {
                     }]
                 });
                 break;
-            case "DefaultWelcomeIntent-applyfilter":
+            case "gt.startConversation-applyfilter":
                 var oppStatus = req.body.queryResult.parameters.oppstatus;
                 console.log("TYPE OS", typeof oppStatus);
                 console.log("opp tstua",oppStatus);
-                console.log("inside DefaultWelcomeIntent-applyfilter ans status is ", oppStatus);
+                console.log("inside gt.startConversation-applyfilter and status is ", oppStatus);
                 res.json({
                     "followupEventInput": {
                         "name": "userquery-event",
